9337352bab272e812041d0cfb7c2705cb3c40e3b
out-stroki-waisman <k5LpLsM5Kgjy>
2022-08-16 16:49:52 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/ivan_vaysman/share_price_api/SharePriceApiApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.ivan_vaysman.share_price_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharePriceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharePriceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yandex/ivan_vaysman/share_price_api/SharePriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.ivan_vaysman.share_price_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharePriceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
