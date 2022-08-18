65c972098695a09173780c811d9de4331b7b4af9
out-stroki-waisman <k5LpLsM5Kgjy>
2022-08-18 11:31:28 +0300

    Added User service and service impl

---
+++ src/main/java/ru/yandex/ivan_vaysman/share_price_api/service/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package ru.yandex.ivan_vaysman.share_price_api.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.stereotype.Service;
+import ru.yandex.ivan_vaysman.share_price_api.domain.model.User;
+import ru.yandex.ivan_vaysman.share_price_api.repository.UserRepository;
+import ru.yandex.ivan_vaysman.share_price_api.service.impl.UserService;
+
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+
+    @Override
+    public User save(User user) {
+        return repository.save(user);
+    }
+
+    @Override
+    public boolean existsUserByEmail(String email){
+        return repository.findByEmail(email) != null;
+    }
+
+    @Override
+    public void prepareUser(DefaultOidcUser defaultOidcUser){
+        String email = defaultOidcUser.getEmail();
+
+        if(!existsUserByEmail(email)){
+            User user = new User();
+            user.setEmail(email);
+            user.setName(defaultOidcUser.getFamilyName() + " " + defaultOidcUser.getGivenName());
+            user.setEmail_verified(defaultOidcUser.getEmailVerified());
+            user.setLocale(defaultOidcUser.getLocale());
+
+            repository.save(user);
+        }
+    }
+}
---
+++ src/main/java/ru/yandex/ivan_vaysman/share_price_api/service/impl/UserService.java
@@ -0,0 +1,10 @@
+package ru.yandex.ivan_vaysman.share_price_api.service.impl;
+
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import ru.yandex.ivan_vaysman.share_price_api.domain.model.User;
+
+public interface UserService {
+    User save(User user);
+    boolean existsUserByEmail(String email);
+    void prepareUser(DefaultOidcUser defaultOidcUser);
+}
